6ed71069462cbfb15756f0e6af925c66f901d0dd
bianca-alex <bianca.lk.alex@outlook.com>
2022-09-13 16:33:47 +0800

    增加字体图标

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
+  <title>@yield('title', 'M-bianca') - Blog</title>
 
   <!-- Styles -->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
