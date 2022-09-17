c6416b8b0e8a4308b85e8f7a1109bb8a988acb5a
bianca-alex <bianca.lk.alex@outlook.com>
2022-09-17 12:25:24 +0800

    admin 配置

---
+++ app/Admin/Controllers/TopicsController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\Topic;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class TopicsController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'Topic';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Topic());
+
+        $grid->column('id', __('Id'));
+        $grid->column('title', __('Title'));
+        $grid->column('body', __('Body'));
+        $grid->column('user_id', __('User id'));
+        $grid->column('category_id', __('Category id'));
+        $grid->column('view_count', __('View count'));
+        $grid->column('order', __('Order'));
+        $grid->column('excerpt', __('Excerpt'));
+        $grid->column('slug', __('Slug'));
+        $grid->column('created_at', __('Created at'));
+        $grid->column('updated_at', __('Updated at'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Topic::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('title', __('Title'));
+        $show->field('body', __('Body'));
+        $show->field('user_id', __('User id'));
+        $show->field('category_id', __('Category id'));
+        $show->field('view_count', __('View count'));
+        $show->field('order', __('Order'));
+        $show->field('excerpt', __('Excerpt'));
+        $show->field('slug', __('Slug'));
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Topic());
+
+        $form->text('title', __('Title'));
+        $form->textarea('body', __('Body'));
+        $form->number('user_id', __('User id'));
+        $form->number('category_id', __('Category id'));
+        $form->number('view_count', __('View count'));
+        $form->number('order', __('Order'));
+        $form->textarea('excerpt', __('Excerpt'));
+        $form->text('slug', __('Slug'));
+
+        return $form;
+    }
+}
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -3,6 +3,9 @@
     <!-- Branding Image -->
   </div>
   <div class="container">
+    <div class="circle">
+      <img src="/images/logo.jpg" alt="love" />
+    </div>
     <a class="navbar-brand" style="margin-left: 82px;" href="{{ url('/') }}">
       @guest
       @else
@@ -10,12 +13,24 @@
       @endguest
     </a>
     <label>{{ $sentence }}</label>
+    @guest
+    @else
     <a href="{{ route('topics.create') }}" style="color: #242020;">
       <i class="fa fa-plus" aria-hidden="true"></i> 
     </a>
+    @endguest 
     <form class="d-flex" role="search">
       <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success" type="submit">Search</button>
     </form>
+    @guest
+    @else
+    <a href="#" onclick="document.getElementById('logout').submit();">
+      <form action="{{ route('logout') }}" method="POST" id="logout">
+        {{ csrf_field() }}
+        <i class="fa fa-sign-out" aria-hidden="true" style="color: #b7c3a0;"></i>
+      </form>
+    </a>
+    @endguest
   </div>
 </nav>
