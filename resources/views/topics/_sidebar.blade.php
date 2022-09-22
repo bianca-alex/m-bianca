26cf5247c2458746f2f44d6d46d47e3b4dae89db
bianca-alex <bianca.lk.alex@outlook.com>
2022-09-22 23:53:17 +0800

    添加侧边栏

--- resources/views/topics/_sidebar.blade.php
+++ resources/views/topics/_sidebar.blade.php
@@ -1,5 +1,35 @@
-<div class="card ">
-  <div class="card-body">
-    右边导航栏
+<div class="card" style="width: 18rem;">
+  <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="{{ asset('images/kobe.jpg') }}" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="{{ asset('images/sidebar.jpg') }}" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="{{ asset('images/life.jpg') }}" class="d-block w-100" alt="...">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
   </div>
+  <div class="card-body">
+    <p class="card-text">Some of life,you have to go to great challenges.</p>
+  </div>
+  <ul class="list-group list-group-flush" style="align-items: center;">
+    <li class="list-group-item">૮꒰ ˶• ༝ •˶꒱ა  记录生活</li>
+    <li class="list-group-item"> ૮(˶ᵔ ᵕ ᵔ˶)ა&nbsp;&nbsp;&nbsp;&nbsp;技术分享</li>
+  </ul>
 </div>
