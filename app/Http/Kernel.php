cdaa34ffe2f417af007b872dc11c58e240cc984d
bianca-alex <bianca.lk.alex@outlook.com>
2022-11-21 22:05:17 +0800

    API 基础配置

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,6 +42,7 @@ class Kernel extends HttpKernel
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
+            \App\Http\Middleware\AcceptHeader::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
