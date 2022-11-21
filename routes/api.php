d14f72102194f33a89c0daea8a0c7a32dcb77f09
bianca-alex <bianca.lk.alex@outlook.com>
2022-11-21 22:11:24 +0800

    频率限制

--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,19 @@ use App\Http\Controllers\Api\VerificationCodesController;
 
 Route::prefix('v1')->name('api.v1.')->group(function(){
 
-    Route::post('verificationCodes', [VerificationCodesController::class, 'store'])
-        ->name('verificationCodes.store');
-    // 用户注册
-    Route::post('users', [UsersController::class, 'store'])
-        ->name('users.store');
+    Route::middleware('throttle:' . config('api.rate_limits.sign'))
+        ->group(function () {
+            // 短信验证码
+            Route::post('verificationCodes', [VerificationCodesController::class, 'store'])
+                ->name('verificationCodes.store');
+
+            // 用户注册
+            Route::post('users', [UsersController::class, 'store'])
+                ->name('users.store');
+        });
+
+    Route::middleware('throttle:' . config('api.rate_limits.access'))
+        ->group(function () {
+
+        });
 });
